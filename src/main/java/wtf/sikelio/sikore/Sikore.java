82659e1bfdfa26faee872905f86be33837d1c2ae
Sik <sikeliodev@gmail.com>
2023-09-19 20:13:56 +0200

    Init

---
+++ src/main/java/wtf/sikelio/sikore/Sikore.java
@@ -0,0 +1,16 @@
+package wtf.sikelio.sikore;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Sikore implements ModInitializer {
+	public static final String MOD_ID = "sikore";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/wtf/sikelio/sikore/SikoreClient.java
@@ -0,0 +1,10 @@
+package wtf.sikelio.sikore;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SikoreClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
